7351c9079c1dd5e3e356a0c60aed8e6299987dfb
Devin Arnold <dbarnold321@gmail.com>
2020-12-13 21:12:14 -0700

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
+# config.middleware.use "Rack::Cors" do
+#     allow do
+#       origins '*'
+#       resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
+#     end
+#   end
