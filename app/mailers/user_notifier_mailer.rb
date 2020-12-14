edc46a3ac594e5a0a18d05b26a8e05d0f42cbae5
Devin Arnold <dbarnold321@gmail.com>
2020-12-13 21:20:57 -0700

    updated cors

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -4,8 +4,9 @@ class UserNotifierMailer < ApplicationMailer
     # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(user)
       @user = user
+      puts(@user, 'check user')
       mail( :to => 'dbarnold321@gmail.com',
-      :subject => 'This is a test email',
+      :subject => 'Website Inquiry',
       :body => "name: #{@user.name}\n
                 email: #{@user.username}\n
                 phone: #{@user.phone}\n
